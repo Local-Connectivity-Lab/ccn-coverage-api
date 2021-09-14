59d25d702372c1be5745527a50948b47c8193532
Mark <sirapop741@gmail.com>
2021-09-14 15:38:05 -0700

    Overhaul the structure

--- src/db/points/points.model.ts
+++
@@ -1,5 +0,0 @@
-import { model } from "mongoose";
-import { IPointDocument } from "./users.types";
-import PointSchema from "./points.schema";
-
-export const pointModel = model<IPointDocument>("point", PointSchema);
--- src/routes.ts
+++
@@ -1,5 +0,0 @@
-import { Express, Request, Response } from "express";
-
-export default function (app: Express) {
-  app.get("/ping", (req: Request, res: Response) => res.sendStatus(200));
-}
