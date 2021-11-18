f4a81570e97aa2cdcd1b3f9090b9979de1d65b5f
Mark <sirapop741@gmail.com>
2021-11-18 00:50:56 -0800

    Finish register endpoint, synced with EPC

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import { json } from 'body-parser'
+import { json, urlencoded, raw } from 'body-parser'
 import { dataRouter } from './routes/data'
 
 // Change this line to match your mongodb server
@@ -8,7 +8,9 @@ const mongodbURI = 'mongodb://localhost:27017/api-data'
 const listeningPort = 3000
 
 const app = express()
-app.use(json())
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(raw({type: 'application/octet-stream', limit : '2mb'}))
 app.use(dataRouter)
 
 
--- src/routes/data.ts
+++ src/routes/data.ts
@@ -1,13 +1,23 @@
 import express, {Request, Response} from 'express'
-import { JSDOM } from 'jsdom'
-import * as L from 'leaflet'
+// import { JSDOM } from 'jsdom'
+// import * as L from 'leaflet'
+import NodeRSA from 'node-rsa'
+const axios = require('axios').default;
+// import axios from 'axios'
 
 import { Data, IData, IQuery, IAggregate, isIAggregate } from '../../models/data'
 
-const dom = new JSDOM('')
-global.window = dom.window as any
-global.document = dom.window.document
-global.navigator = dom.window.navigator
+const EPC_API_ENDPOINT = 'http://localhost:3001/api/'
+
+const instance = axios.create({
+  baseURL: EPC_API_ENDPOINT,
+  timeout: 1000,
+});
+
+// const dom = new JSDOM('')
+// global.window = dom.window as any
+// global.document = dom.window.document
+// global.navigator = dom.window.navigator
 
 const router = express.Router()
 
@@ -38,21 +48,21 @@ router.get('/api/agg', async (req: Request, res: Response) => {
   if (!isIAggregate(req.query)) {
     throw new Error('...')
   }
-  const reqData: IAggregate = req.query
+  // const reqData: IAggregate = req.query
 
-  const map = L.map(dom.window.document.createElement('div')).setView([0, 0], 10 /* zoom level */)
-  // const unproject0 = map.unproject([0, 0], 10 /* zoom */)
-  // const lat0 = unproject0.lat;
-  // const lng0 = unproject0.lng;
-  // const unproject1 = map.unproject([1, 1], 10 /* zoom */)
-  // const lat1 = unproject1.lat;
-  // const lng1 = unproject1.lng;
-  const {lat: lat0, lng: lng0} = map.unproject([0, 0], 10 /* zoom */)
-  const {lat: lat1, lng: lng1} = map.unproject([1, 1], 10 /* zoom */)
-  const binSizeX = Math.abs(lng1 - lng0)
-  const binSizeY = Math.abs(lat1 - lat0)
+  // const map = L.map(dom.window.document.createElement('div')).setView([0, 0], 10 /* zoom level */)
+  // // const unproject0 = map.unproject([0, 0], 10 /* zoom */)
+  // // const lat0 = unproject0.lat;
+  // // const lng0 = unproject0.lng;
+  // // const unproject1 = map.unproject([1, 1], 10 /* zoom */)
+  // // const lat1 = unproject1.lat;
+  // // const lng1 = unproject1.lng;
+  // const {lat: lat0, lng: lng0} = map.unproject([0, 0], 10 /* zoom */)
+  // const {lat: lat1, lng: lng1} = map.unproject([1, 1], 10 /* zoom */)
+  // const binSizeX = Math.abs(lng1 - lng0)
+  // const binSizeY = Math.abs(lat1 - lat0)
   
-  console.log(reqData)
+  // console.log(reqData)
 
   // let data
   // let findObj:any = {}
@@ -107,6 +117,42 @@ router.post('/api/data', async (req: Request, res: Response) => {
     }
     return res.status(201).send("Successful")
   }
+})
+
+// TODO: Work with hardware backed attestation
+router.post('/api/register', async (req: Request, res: Response) => {
+  const buffer:Buffer = req.body
+  var public_key:Buffer = buffer.slice(0, 270)
+  var identity:Buffer = buffer.slice(270, 270 + 32)
+  var attestation = buffer.slice(270 + 32, 270 + 32 + 256)
+  // Check if the signature exists
+  if (!('signature' in req.headers) || typeof req.headers['signature'] != 'string') {
+    throw new Error('...')
+  }
+  var signature = req.headers['signature']
+  // console.log(public_key.toString('base64'))
+  // console.log(identity.toString('base64'))
+  // console.log(attestation.toString('base64'))
+  // console.log(signature)
+
+  // Verify the signature
+  var key = new NodeRSA()
+  key.importKey(public_key, 'pkcs1-public-der')
+  if (!key.verify(buffer, signature, 'buffer', 'base64')) {
+    res.send('Invalid signature')
+    return;
+  }
+
+  // Asking EPC's database to register a new user
+  
+  const response = await instance.post('register', 
+    {
+      'identity': identity.toString('hex'),
+      'public_key': key.exportKey('pkcs1-public-pem')
+    }
+  )
+
+  res.send(response.data);
 })
 
 
