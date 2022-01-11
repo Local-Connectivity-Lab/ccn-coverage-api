26cba0fc9c4baa006b43cce004c5cf6d54008462
Mark <sirapop741@gmail.com>
2022-01-11 01:23:44 -0800

    Swapped mongourl

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,9 @@ import { queryRouter } from './routes/query'
 import { ldapRouter } from './routes/ldap-login'
 
 // Change this line to match your mongodb server
+const mongodbURI = 'mongodb://localhost:27017/api-data'
 // const mongodbURI = 'mongodb://localhost:27017/api-data'
-// const mongodbURI = 'mongodb://localhost:27017/api-data'
-const mongodbURI = 'mongodb://192.168.249.129:27017/api-data'
+// const mongodbURI = 'mongodb://192.168.249.129:27017/api-data'
 const listeningPort = 3000
 
 const app = express()
