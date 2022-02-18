e71e74d2e4b6814cdd826f3326718938009c6433
Mark <sirapop741@gmail.com>
2022-02-18 10:26:10 -0800

    Use passport-session for session management

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,9 @@ import { editSitesRouter } from './routes/edit-sites'
 
 // Change this line to match your mongodb server
 // TODO: Work with ENV
+const mongodbURI = 'mongodb://localhost:27017/api-data'
 // const mongodbURI = 'mongodb://localhost:27017/api-data'
-// const mongodbURI = 'mongodb://localhost:27017/api-data'
-const mongodbURI = 'mongodb://192.168.249.129:27017/api-data'
+// const mongodbURI = 'mongodb://192.168.249.129:27017/api-data'
 const listeningPort = 3000
 
 const app = express()
