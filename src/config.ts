a05b86fc5520218a6d9e7334ad7d2eb01b2ff345
Sirapop Theeranantachai <sirapop741@gmail.com>
2024-03-27 22:29:55 -0700

    ldap: update url and base dn

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export default {
   ldap: {
-    dn: 'cn=users,cn=accounts,dc=seattlecommunitynetwork,dc=org',
-    url: 'ldap://ldap.seattlecommunitynetwork.org'
+    dn: 'cn=users,cn=accounts,dc=cloud,dc=seattlecommunitynetwork,dc=org',
+    url: 'ldap://10.0.1.4'
   },
   mongodbURI : 'mongodb://localhost:27017/api-data',
 }
